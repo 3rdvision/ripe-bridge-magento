dd6fb2e573c3070098a564453c4a572718ca43c9
3rdvision <kazgru@gmail.com>
2022-02-18 18:34:39 +0000

    small fix

--- view/frontend/web/js/button.js
+++ view/frontend/web/js/button.js
@@ -1,5 +1,5 @@
 define(["jquery"], ($) => {
-    const BASE_URL = "http://localhost:8080";
+    const BASE_URL = "https://bridge.platforme.com";
 
     /**
      * Handles the provided error according to the default strategy,
